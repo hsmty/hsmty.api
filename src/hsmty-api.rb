27013ce8c2ae80d4ca27dcb42a081903e896a034
R. Diaz de Leon <leon@elinter.net>
2013-08-02 09:44:37 -0500

    Serving basic status.json with sinatra

--- src/hsmty-api.rb
+++ src/hsmty-api.rb
@@ -1,7 +1,30 @@
 
 require 'sinatra'
+require 'json'
+require 'sequel'
 
 get '/status.json' do
-  '{ "api": 0.13, "space": "HackerspaceMTY" }'
+
+    status = createstatus()
+
+    status.to_json
 end
 
+def createstatus()
+    dbh = getdbh();
+
+    file = open('status.json')
+    status = JSON.parse(file.read)
+
+    if (status and status['state']) then
+        status['state'][:open] = true
+    else
+        status = {}
+    end
+
+    return status
+end
+
+def getdbh()
+    dbh = Sequel.sqlite('/tmp/hsmty.db')
+end
