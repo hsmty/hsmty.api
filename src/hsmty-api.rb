33065cd786c952e1330a6a388d34ea9a20964a65
R. Diaz de Leon <leon@elinter.net>
2013-08-02 11:43:52 -0500

    Added error handling and some code for the devices

--- src/hsmty-api.rb
+++ src/hsmty-api.rb
@@ -3,6 +3,14 @@ require 'sinatra'
 require 'json'
 require 'sequel'
 
+error 400 do |mesg|
+    if mesg then
+        'Bad Request: #{mesg}'
+    else
+        'Bad Request'
+    end
+end
+
 get '/status.json' do
 
     status = createstatus()
@@ -10,8 +18,46 @@ get '/status.json' do
     status.to_json
 end
 
+get '/idevices/?' do
+    db = getdbh()
+    count = db[:devices].count
+    return count.to_s
+end
+
+get '/idevices/:token' do |token|
+    db = getdbh()
+    device = db[:devices].where(:token => token).first
+    if device then
+        'found'
+    else
+        status 404
+        body 'Device not found'
+    end
+end
+
+put '/idevices/:token' do |token|
+    reg = JSON.parse(request.body.read)
+    if (reg and reg['id'] and reg['key']) then
+        db = getdbh()
+        db[:devices].insert(
+            :id  => reg['id'],
+            :key => reg['key'],
+            :token => tokey,
+            :version => 0
+        )
+        
+    elsif (reg)
+        status 400
+        body 'Missing params'
+    else
+        status 400
+        body 'Invalid Request'
+    end
+
+end
+
 def createstatus()
-    db = getdbh();
+    db = getdbh()
 
     file = open('status.json')
     status = JSON.parse(file.read)
