78eac97191ecc83d63b5f1a392725168a018c61c
R. Diaz de Leon <leon@elinter.net>
2013-08-19 14:05:27 -0500

    Added tests for updated status

--- t/basic.rb
+++ t/basic.rb
@@ -2,6 +2,8 @@ require 'hsmty/api'
 require 'test/unit'
 require 'rack/test'
 
+require 'json'
+
 class APITest < Test::Unit::TestCase
     include Rack::Test::Methods
 
@@ -21,10 +23,17 @@ class APITest < Test::Unit::TestCase
     end
 
     def test_update_status
-        post '/status/update'
+        post '/status/update', :status => 'open'
         assert_equal 401, last_response.status
         authorize 'admin', 'admin'
         post '/status/update', :status => 'open'
         assert_equal 200, last_response.status
+        get '/status.json'
+        status = JSON.parse(last_response.body)
+        assert status['state']['open'], "Didn't update status as open"
+        post '/status/update', :status => 'close'
+        get '/status.json'
+        status = JSON.parse(last_response.body)
+        assert_equal false, status['state']['open'], "Didn't update status as close"
     end
 end
