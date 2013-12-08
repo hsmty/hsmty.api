d0d766fa481e8c4b8214908e00c2facb566be33a
R. Diaz de Leon <leon@elinter.net>
2013-12-08 00:30:23 -0600

    Added files to daemonize the server

---
+++ init.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -e
+
+### BEGIN INIT INFO
+# Provides:		hsmty-api
+# Required-Start:	$local_fs $remote_fs $network $time
+# Required-Stop:	$local_fs $remote_fs $network $time
+# Should-Start:		$syslog
+# Should-Stop:		$syslog
+# Default-Start:	2 3 4 5
+# Default-Stop:		0 1 6
+# Short-Description:	HSMTY Web API server
+### END INIT INFO
+
+RACK_ENV=production
+
+cd /var/www/hsmty_api
+sudo -u www-data bundle exec ruby lib/hsmty/control.rb $1
+RETVAL=$?
+
+exit $RETVAL
---
+++ lib/hsmty/control.rb
@@ -0,0 +1,12 @@
+require 'rubygems'
+require 'daemons'
+
+DIR="/var/www/hsmty_api"
+prog="lib/hsmty/minimal.rb"
+
+ENV["RACK_ENV"] = "production"
+
+Daemons.run_proc(prog) do
+  Dir.chdir(DIR)
+  exec "bundle exec ruby #{prog} >> /var/log/hsmty_api.log 2>&1"
+end
